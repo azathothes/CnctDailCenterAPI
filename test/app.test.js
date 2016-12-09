e577af97600430a443b860964df3ef7667b5f9fd
wanlf <azathothes@outlook.com>
2016-12-09 17:59:31 +0800

    bug fix

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,16 @@
 const expect = require('chai').expect;
 const app = require('../app.js');
 const supertest = require('supertest')(app);
-var url = '/THZTTS?CallNo=12232&CalledNo=12232212232&CallSheetID=asdsadsasdsda-asdas-asd-asd&CallType=normal&Ring=30&Begin=2016/2/3 12:33&End=123&QueueTime=232&Agent=testagent&Exten=testExten&Queue=testQueue&State=testState&RecordFile=testfile.mp3&FileServer=c:/test/kk&Province=四川&District=成都';
+var url = '/THZTTS?CallNo=12232&CalledNo=12232212232&CallSheetID=asdsadsasdsda-asdas-asd-asd&CallType=normal&Ring=30&Begin=2016/2/3 12:33&End=123&QueueTime=232&Agent=testagent&Exten=testExten&Queue=testQueue&State=testState&RecordFile=testfile.mp3&FileServer=c:/test/kk&Province=四川&District=遂宁';
 describe("http get /THZTTS test",function(){
     it("correct request data && correct request method should be fine.",function(done){
         supertest
         .get(url)
-        .expect(207,done);
+        .expect(200)
+        .end(function(err, res) {
+            if (err) throw err;
+            expect(res).to.be.equal("s");
+        });
     });
 
     
